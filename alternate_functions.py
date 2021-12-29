55d6b071de96ab0fa671cc8686558db2f548aa3a
kajode <59487189+kajode@users.noreply.github.com>
2021-12-29 17:44:35 +0100

    ref link

--- alternate_functions.py
+++ alternate_functions.py
@@ -92,9 +92,12 @@ def check_price(card):
         ut.write_price(card_type, card_price)
         ut.mysql_update(card_type, card_price, link, shop_name)
 
+        #get ref link
+        link = ut.create_reflink(link.replace('\n',''))
+
         if card_price < card_max_price:
             print(card_fullname)
-            card_deals.append([card_type, card_price, card_fullname, link.replace('\n',''), shop_name])
+            card_deals.append([card_type, card_price, card_fullname, link,  shop_name])
 
     return card_deals
 
